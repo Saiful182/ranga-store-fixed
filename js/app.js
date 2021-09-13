ecf9c08e578d7cfd61b7c5bfc0cf9e773296ce79
Saiful182 <saifulislamcse0@gamil.com>
2021-09-13 14:44:22 +0600

    all done

--- js/app.js
+++ js/app.js
@@ -94,7 +94,7 @@ const productDetails = (id) => {
 
     div.innerHTML = `
     <h2>${iteam.title}</h2>
-    <h3>Total product sold:${totalSell}</h3>
+    <h2>Total product sold:${totalSell}</h2>
       <p>${description}</p>
     `;
     divParent.appendChild(div);
